fe0f81a3c85cfb35c368938e4bad207e384adde9
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-08-22 14:59:34 -0300

    Fix formatting

--- strings/concatenate-strings/concatenate-using-join.go
+++ strings/concatenate-strings/concatenate-using-join.go
@@ -5,16 +5,16 @@ import "strings"
 
 func main() {
 
-  //Texts to be concatenated
-  firstname := "Alexandre"
-  lastname  := "Gama"
+	//Texts to be concatenated
+	firstname := "Alexandre"
+	lastname := "Gama"
 
-  //Creating a slice with texts
-  text := []string{firstname, lastname}
-  //Using strings.Join() method to join texts. Notice that this method will return a new string object
-  newText := strings.Join(text, "")
-  //This should print just the slice, without joining the texts
-  fmt.Println(text)
-  //This should print the joined text
-  fmt.Println(newText)
+	//Creating a slice with texts
+	text := []string{firstname, lastname}
+	//Using strings.Join() method to join texts. Notice that this method will return a new string object
+	newText := strings.Join(text, "")
+	//This should print just the slice, without joining the texts
+	fmt.Println(text)
+	//This should print the joined text
+	fmt.Println(newText)
 }
