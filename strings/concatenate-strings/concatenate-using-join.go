4d9852ad2676727832ed3c79fb7c807373c8d780
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-08-22 14:52:52 -0300

    Adding an example of how to concatenate 2 strings using Join from strings package

---
+++ strings/concatenate-strings/concatenate-using-join.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "strings"
+
+func main() {
+
+  //Texts to be concatenated
+  firstname := "Alexandre"
+  lastname  := "Gama"
+
+  //Creating a slice with texts
+  text := []string{firstname, lastname}
+  //Using strings.Join() method to join texts. Notice that this method will return a new string object
+  newText := strings.Join(text, "")
+  //This should print just the slice, without joining the texts
+  fmt.Println(text)
+  //This should print the joined text
+  fmt.Println(newText)
+}
